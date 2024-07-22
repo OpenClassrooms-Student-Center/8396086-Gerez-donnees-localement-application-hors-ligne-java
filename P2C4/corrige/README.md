fd5de1d1476ff2bb159cf204861d4711c3a66d5e
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-07-22 22:59:19 +0200

    Add P2C4

---
+++ P2C4/corrige/README.md
@@ -0,0 +1,7 @@
+# Éléments de correction
+
+Dans le cadre de cet exercice, 2 fichiers ont été modifiés et 1 fichier a été créé.
+
+Les classes `Animal` et `Vaccine` qui permettent de modéliser les tables des animaux et des vaccins dans la base de données ont été annotés.
+
+Le convertisseur de types `Converters` qui permet d'indiquer à Room comment gérer les dates d'injection des vaccins a été créé.
---
+++ P2C4/enonce/app/src/main/java/com/openclassrooms/room/data/entity/Animal.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.room.data.entity;
+
+final public class Animal
+{
+
+  enum Type
+  {
+    CAT, DOG, RABBIT
+  }
+
+  enum Race
+  {
+    PERSIAN, SIEMESE,
+    GERMAN_SHEPHERD, GOLDEN_RETRIEVER,
+    DWARF_RABBIT, FLEMISH_RABBIT
+  }
+
+  public final long id;
+
+  public final Type type;
+
+  public final Race race;
+
+  public final double weight;
+
+  public final double height;
+
+  public final String color;
+
+  public Animal(long id, Type type, Race race, double weight, double height, String color)
+  {
+    this.id = id;
+    this.type = type;
+    this.race = race;
+    this.weight = weight;
+    this.height = height;
+    this.color = color;
+  }
+  
+}
